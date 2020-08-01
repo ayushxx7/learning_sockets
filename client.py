058b26669b830de48405f3f819b2638af4c753f5
Ayush <ayushxx7@gmail.com>
2020-08-01 14:37:18 +0530

    adding headersize functionality, so the socket connect is not closed as soon as the message is sent by the server

--- client.py
+++ client.py
@@ -3,12 +3,25 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
+HEADER_SIZE = 10
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode('utf-8')
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(8)
+        if new_msg:
+            print(f"new mssage len: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+
+        full_msg += msg.decode('utf-8')
+
+        if len(full_msg)-HEADER_SIZE == msglen:
+            print("Full message received")
+            print("Message:",full_msg[HEADER_SIZE:])
+            full_msg = ''
+            break
+
+    print(full_msg)
     # print(msg.decode('utf-8'))
 
--- server.py
+++ server.py
@@ -9,11 +9,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 
-
+HEADER_SIZE = 10
 while True: #listen forever.
     clieentsocket, address = s.accept()
     print(f'Connection from {address} has been established!')
-    clieentsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clieentsocket.close()
+    msg = 'Welcome to the server!'
+    msg = f'{len(msg):<{HEADER_SIZE}}'+msg
+
+    clieentsocket.send(bytes(msg, "utf-8"))
 
 
